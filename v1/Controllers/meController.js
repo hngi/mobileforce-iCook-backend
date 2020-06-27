d0ba5091b3126ef26447bf108f9f7643034c2c24
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-27 18:19:51 +0100

    add my auth route

--- v1/Controllers/meController.js
+++ v1/Controllers/meController.js
@@ -28,6 +28,27 @@ exports.get_me = async (req, res) => {
 };
 
 exports.get_auth = async (req, res) => {
+  try {
+    const userId = req.user._id.toString(); 
+    const me = await User.findOne({_id: userId});
+
+    if(me){
+      res.status(200).json({
+        status: 'success',
+        error: '',
+        data: {
+          me
+        }
+      });
+    } else{
+      throw new Error('Not found');
+    }
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      error: `user with ID ${req.params.id} not found`,
+    });
+  }
 };
 
 exports.get_favourites = async (req, res) => {
