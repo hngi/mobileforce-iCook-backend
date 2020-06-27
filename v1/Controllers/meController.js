46563e17b618e66bc2ed0ca7a44ded06395d4526
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-27 18:27:44 +0100

    chore

--- v1/Controllers/meController.js
+++ v1/Controllers/meController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const User = require('../../Models/authModel');
 const Dish = require('../../Models/dishModel');
 const uploadImage = require('../../Database/uploadImage');
@@ -52,6 +53,33 @@ exports.get_auth = async (req, res) => {
 };
 
 exports.get_favourites = async (req, res) => {
+  try {
+    const userId = req.user._id.toString(); 
+    const me = await Profile.findOne({userId});
+    const favourites = await Dish.find({
+      '_id': {
+        $in: me.favourites.map(id => mongoose.Types.ObjectId(id.toString()))
+      }
+    });
+
+    if(me){
+      res.status(200).json({
+        status: 'success',
+        error: '',
+        data: {
+          count: favourites.length,
+          favourites
+        }
+      });
+    } else{
+      throw new Error('Not found');
+    }
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      error: `user with ID ${req.params.id} not found`,
+    });
+  }
 
 };
 
