86558e8d958dc7ea465d4cf4362aa6cc1141a41f
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-27 13:47:59 +0100

    add/search route

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const express = require("express");
 require('./Database/database');
 
 // const bodyParser = require("body-parser");
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 
-app.use("/api/v1/users", require("./v1/Routes/userRoute"))
-app.use("/api/v1/dishes", require("./v1/Routes/dishRoute"))
-app.use("/api/v1/authenticate", require("./v1/Routes/authRoute"))
+app.use("/api/v1/users", require("./v1/Routes/userRoute"));
+app.use("/api/v1/dishes", require("./v1/Routes/dishRoute"));
+app.use("/api/v1/authenticate", require("./v1/Routes/authRoute"));
+app.use("/api/v1/search", require("./v1/Routes/searchRoute"));
 
 app.use((err, req, res, next) => {
   console.log(err);
---
+++ v1/Controllers/searchController.js
@@ -0,0 +1,27 @@
+const User = require('../../Models/authModel');
+const Dish = require('../../Models/dishModel');
+const Profile = require("../../Models/profileModel");
+
+exports.search_dish = async (req, res) => {
+  const query = req.query;
+  const { name } = query;
+  const _dishes = await Dish.find({name});
+  const dishes = _dishes.map(dish => {
+    const d = Object.assign({}, {
+      ...dish.toJSON(),
+      likesCount: dish.likes.length,
+      isLiked : dish._isLiked(req.user._id)
+    });
+    delete d.likes;
+    delete d.comments;
+    return d;
+  });
+  res.status(200)
+    .json({ 
+      status: "success",
+      error: "",
+      data: {
+        dishes
+      }
+    });
+};
