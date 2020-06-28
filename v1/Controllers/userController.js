a34b586d5ba53fe274ceb1326ad8953a4421145f
demola07 <demolasobaki@gmail.com>
2020-06-28 15:55:37 +0100

    fixes #34

--- v1/Controllers/userController.js
+++ v1/Controllers/userController.js
@@ -1,229 +1,203 @@
-const User = require('../../Models/authModel');
-const Dish = require('../../Models/dishModel');
-const uploadImage = require('../../Database/uploadImage');
-const Profile = require('../../Models/profileModel');
-const PublicResponse = require('../../Helpers/model');
+const User = require('../../Models/authModel')
+const Dish = require('../../Models/dishModel')
+const uploadImage = require('../../Database/uploadImage')
+const Profile = require('../../Models/profileModel')
+const PublicResponse = require('../../Helpers/model')
 
 exports.get_all_users = async (req, res, next) => {
   try {
     // const users = await User.find().populate('profile').select('_id method local.email');
-    const users = await Profile.find().select('-email').populate('dishes');
+    const users = await Profile.find().select('-email').populate('dishes')
     res.status(200).json({
       status: 'success',
       error: '',
       results: users.length,
       data: {
-        users: PublicResponse.users(users, req),
-      },
-    });
+        users: PublicResponse.users(users, req)
+      }
+    })
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      error: err.message,
-    });
+      error: err.message
+    })
   }
-};
+}
 
 exports.get_user_by_id = async (req, res, next) => {
   try {
-    const userId = req.params.id;
+    const userId = req.params.id
     const user = await Profile.findOne({ userId })
       .select(['-email', '-favourites'])
-      .populate('dishes');
-    const _user = PublicResponse.user(user, req);
+      .populate('dishes')
+    const _user = PublicResponse.user(user, req)
 
     if (user) {
       res.status(200).json({
         status: 'success',
         error: '',
         data: {
-          user: _user,
-        },
-      });
+          user: _user
+        }
+      })
     } else {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
   } catch (err) {
     return res.status(404).json({
       status: 'fail',
-      error: `user with ID ${req.params.id} not found`,
-    });
+      error: `user with ID ${req.params.id} not found`
+    })
   }
-};
-
-// PUT /api/v1/users
-exports.updateUserDetails = async (req, res, next) => {
-  try {
-    const fieldsToUpdate = {
-      name: req.body.name,
-      email: req.body.email,
-      gender: req.body.gender,
-      phoneNumber: req.body.phone,
-    };
-
-    const user = await Profile.findOneAndUpdate({ userId: req.user._id }, fieldsToUpdate, {
-      new: true,
-    });
-
-    res.status(200).json({
-      status: 'success',
-      data: user,
-      error: {},
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err,
-    });
-  }
-};
+}
 
 // /api/users/id/followers/:id - get
 exports.get_followers = async (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
 
   try {
-    const user = await Profile.findById(id);
+    const user = await Profile.findById(id)
 
     if (!user) {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
 
-    const followers = user.followers;
+    const followers = user.followers
 
     res.status(200).json({
       status: 'success',
       count: followers.length,
       data: followers,
-      error: '',
-    });
+      error: ''
+    })
   } catch (err) {
     return res.status(404).json({
       status: 'fail',
-      message: 'User not found',
-    });
+      message: 'User not found'
+    })
   }
-};
+}
 
 // /api/users/following/:id - get
 exports.get_following = async (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   try {
-    const user = await Profile.findById(id);
+    const user = await Profile.findById(id)
 
     if (!user) {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
 
-    const following = user.following;
+    const following = user.following
 
     res.status(200).json({
       status: 'success',
       count: following.length,
       data: following,
-      error: '',
-    });
+      error: ''
+    })
   } catch (err) {
     return res.status(404).json({
       status: 'fail',
-      message: 'User not found',
-    });
+      message: 'User not found'
+    })
   }
-};
+}
 // /api/users/follow/:id - put
 exports.followUser = async (req, res, next) => {
   try {
-    const followId = req.params.id.toString();
-    const id = req.user._id.toString();
+    const followId = req.params.id.toString()
+    const id = req.user._id.toString()
 
     if (id === followId) {
-      throw new Error('You cant follow your self');
+      throw new Error('You cant follow your self')
     }
 
-    const userProfile = await Profile.findOne({ userId: id });
-    const profileId = userProfile._id;
-    const following = userProfile.following;
+    const userProfile = await Profile.findOne({ userId: id })
+    const profileId = userProfile._id
+    const following = userProfile.following
 
-    const isMatch = following.some((fol) => fol == followId);
+    const isMatch = following.some((fol) => fol == followId)
 
     if (isMatch) {
       // idempotent mutation
       return res.status(200).json({
         status: 'fail',
-        message: `You are already following user with ID ${followId}`,
-      });
+        message: `You are already following user with ID ${followId}`
+      })
     }
 
     await Profile.findByIdAndUpdate(
       followId,
       {
-        $push: { followers: profileId },
+        $push: { followers: profileId }
       },
       { new: true }
-    );
+    )
 
     await Profile.findByIdAndUpdate(
       profileId,
       {
-        $push: { following: followId },
+        $push: { following: followId }
       },
       { new: true }
-    );
+    )
 
     res.status(200).json({
       status: 'success',
       message: `You have successfully followed user with ID ${followId} `,
-      error: '',
-    });
+      error: ''
+    })
   } catch (err) {
-    return res.status(400).json({ status: 'fail', error: err.message });
+    return res.status(400).json({ status: 'fail', error: err.message })
   }
-};
+}
 
 // /api/users/unfollow/:id - put
 exports.unfollowUser = async (req, res, next) => {
   try {
-    const unfollowId = req.params.id.toString();
-    const id = req.user._id.toString();
+    const unfollowId = req.params.id.toString()
+    const id = req.user._id.toString()
 
     if (id === unfollowId) {
-      throw new Error('You cant unfollow your self');
+      throw new Error('You cant unfollow your self')
     }
 
-    const userProfile = await Profile.findOne({ userId: id });
-    const profileId = userProfile._id;
-    const following = userProfile.following;
+    const userProfile = await Profile.findOne({ userId: id })
+    const profileId = userProfile._id
+    const following = userProfile.following
 
-    const isMatch = following.find((fol) => fol == unfollowId);
+    const isMatch = following.find((fol) => fol == unfollowId)
 
     if (!isMatch) {
       // idempotent mutation
       return res.status(200).json({
         status: 'fail',
-        message: `You have already unfollowed user with ID ${unfollowId}`,
-      });
+        message: `You have already unfollowed user with ID ${unfollowId}`
+      })
     }
 
     await Profile.findByIdAndUpdate(
       unfollowId,
       {
-        $pull: { followers: profileId },
+        $pull: { followers: profileId }
       },
       { new: true }
-    );
+    )
     await Profile.findByIdAndUpdate(
       profileId,
       {
-        $pull: { following: unfollowId },
+        $pull: { following: unfollowId }
       },
       { new: true }
-    );
+    )
 
     res.status(200).json({
       success: true,
       message: `You have successfully unfollowed user with ID ${unfollowId} `,
-      error: '',
-    });
+      error: ''
+    })
   } catch (err) {
-    return res.status(400).json({ status: 'fail', error: err.message });
+    return res.status(400).json({ status: 'fail', error: err.message })
   }
-};
+}
--- v1/Routes/userRoute.js
+++ v1/Routes/userRoute.js
@@ -1,32 +1,29 @@
-const express = require('express');
-const router = express.Router();
-const user_controller = require('../Controllers/userController');
+const express = require('express')
+const router = express.Router()
+const user_controller = require('../Controllers/userController')
 // const multer = require('multer');
 // const storage = multer.memoryStorage();
 // const upload = multer({ storage: storage });
 
-const passport = require('passport');
-const passportJWT = passport.authenticate('jwt', { session: false });
+const passport = require('passport')
+const passportJWT = passport.authenticate('jwt', { session: false })
 
 // get all users
-router.get('/', passportJWT, user_controller.get_all_users);
+router.get('/', passportJWT, user_controller.get_all_users)
 
 //get user by id
-router.get('/:id', passportJWT, user_controller.get_user_by_id);
-
-//put update user details
-router.patch('/', passportJWT, user_controller.updateUserDetails);
+router.get('/:id', passportJWT, user_controller.get_user_by_id)
 
 // follow a user
-router.put('/follow/:id', passportJWT, user_controller.followUser);
+router.put('/follow/:id', passportJWT, user_controller.followUser)
 
 // unfollow a user
-router.put('/unfollow/:id', passportJWT, user_controller.unfollowUser);
+router.put('/unfollow/:id', passportJWT, user_controller.unfollowUser)
 
 // get followers
-router.get('/followers/:id', passportJWT, user_controller.get_followers);
+router.get('/followers/:id', passportJWT, user_controller.get_followers)
 
 // get following
-router.get('/following/:id', passportJWT, user_controller.get_following);
+router.get('/following/:id', passportJWT, user_controller.get_following)
 
-module.exports = router;
+module.exports = router
