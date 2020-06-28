ef8443ce653316c81dad81a9df89d2dfa2a1506f
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-28 20:44:59 +0100

    fix/warning

--- Database/database.js
+++ Database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }
 );
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
