15949afcc9d5f3fe51383d656e7e099eb325b1ae
demola07 <demolasobaki@gmail.com>
2020-06-28 16:01:33 +0100

    fixes #44

--- v1/Controllers/dishController.js
+++ v1/Controllers/dishController.js
@@ -1,228 +1,234 @@
-const Dish = require('../../Models/dishModel');
-const uploadImage = require('../../Database/uploadImage');
-const Profile = require('../../Models/profileModel');
-const User = require('../../Models/authModel');
-const PublicResponse = require('../../Helpers/model');
+const Dish = require('../../Models/dishModel')
+const uploadImage = require('../../Database/uploadImage')
+const Profile = require('../../Models/profileModel')
+const User = require('../../Models/authModel')
+const PublicResponse = require('../../Helpers/model')
 
 exports.createDish = async (req, res, next) => {
   try {
-    const { name, recipe, healthBenefits, ingredients } = req.body;
+    const { name, recipe, healthBenefits, ingredients } = req.body
 
-    const userId = req.user._id;
+    const userId = req.user._id
 
     const dish = new Dish({
       name: name,
       recipe: recipe,
       healthBenefits: healthBenefits,
       ingredients: ingredients,
-      chefId: userId,
-    });
+      chefId: userId
+    })
 
-    const findProfile = await User.findById(userId).populate('profile');
+    const findProfile = await User.findById(userId).populate('profile')
 
-    const profileId = findProfile.profile[0]._id;
+    const profileId = findProfile.profile[0]._id
 
     await Profile.findByIdAndUpdate(
       profileId,
       { $push: { dishes: dish } },
       { new: true, useFindAndModify: false }
-    );
-    await dish.save();
+    )
+    await dish.save()
     return res.status(201).json({
       status: 'success',
       error: '',
       message: 'dish saved successfully!',
       data: {
-        dish,
-      },
-    });
+        dish
+      }
+    })
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      error: error.message,
-    });
+      error: error.message
+    })
   }
-};
+}
 
 exports.get_all_dishes = async (req, res, next) => {
   try {
-    const _dishes = await Dish.find({ chefId: req.user._id });
+    const _dishes = await Dish.find({ chefId: req.user._id })
     const me = await Profile.findOne({
-      userId: req.user._id,
-    });
-    const isFavourite = (id) => ({ isFavourite: me.favourites.includes(id) });
-    const dishes = PublicResponse.dishes(_dishes, req, isFavourite);
+      userId: req.user._id
+    })
+    const isFavourite = (id) => ({ isFavourite: me.favourites.includes(id) })
+    const dishes = PublicResponse.dishes(_dishes, req, isFavourite)
     return res.status(200).json({
       status: 'success',
       error: '',
       results: dishes.length,
       data: {
-        dishes,
-      },
-    });
+        dishes
+      }
+    })
   } catch (error) {
     return res.status(404).json({
       status: 'fail',
-      error: error.message,
-    });
+      error: error.message
+    })
   }
-};
+}
 
 exports.get_dishes_by_ID = async (req, res, next) => {
   try {
-    const dish = await Dish.findById(req.params.id);
+    const dish = await Dish.findById(req.params.id)
     if (dish) {
       const me = await Profile.findOne({
-        userId: req.user._id,
-      });
-      const isFavourite = me.favourites.includes(req.params.id);
-      const d = PublicResponse.dish(dish, req, { isFavourite });
+        userId: req.user._id
+      })
+      const isFavourite = me.favourites.includes(req.params.id)
+      const d = PublicResponse.dish(dish, req, { isFavourite })
       res.status(200).json({
         status: 'success',
         error: '',
         data: {
-          dish: d,
-        },
-      });
+          dish: d
+        }
+      })
     } else {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
   } catch (error) {
     return res.status(404).json({
       status: 'fail',
-      error: `dish with ID ${req.params.id} not found`,
-    });
+      error: `dish with ID ${req.params.id} not found`
+    })
   }
-};
+}
 
 // Edit PUT Dish api/v1/dishes/:id
 exports.edit_dish = async (req, res, next) => {
+  const { name, recipe, healthBenefits, ingredients } = req.body
+  const fieldsToUpdate = {}
+  if (name) fieldsToUpdate.name = name
+  if (recipe) fieldsToUpdate.recipe = recipe
+  if (healthBenefits) fieldsToUpdate.healthBenefits = healthBenefits
+  if (ingredients) fieldsToUpdate.ingredients = ingredients
+
   try {
-    const userId = req.user._id.toString();
-    const dishId = req.params.id;
+    const userId = req.user._id.toString()
+    const dishId = req.params.id
 
-    const updatedDish = {
-      name: req.body.name,
-      recipe: req.body.recipe,
-      healthBenefits: req.body.healthBenefits,
-      ingredients: req.body.ingredients,
-    };
-
-    const dish = await Dish.findOne({ _id: dishId });
+    let dish = await Dish.findOne({ _id: dishId })
     if (!dish) {
-      throw new Error('Dish not found');
+      throw new Error('Dish not found')
     }
 
     if (dish.chefId.toString() !== userId) {
-      throw new Error('Unauthorized');
+      throw new Error('Unauthorized')
     }
 
-    const data = await Dish.findByIdAndUpdate(dishId, updatedDish, { new: true });
+    dish = await Dish.findOneAndUpdate(
+      { chefId: req.user._id },
+      { $set: fieldsToUpdate },
+      {
+        new: true
+      }
+    )
 
     res.status(200).json({
       status: 'success',
       error: '',
-      data,
-    });
+      data: dish
+    })
   } catch (error) {
-    const code = error.message === 'Unauthorized' ? 403 : 400;
+    const code = error.message === 'Unauthorized' ? 403 : 400
     return res.status(code).json({
       status: 'fail',
-      error: error.message,
-    });
+      error: error.message
+    })
   }
-};
+}
 
 // Delete operation should be idempotent
 // @Usman Jun 27
 exports.delete_dish = async (req, res, next) => {
   try {
-    const dish = await Dish.findOne({ _id: req.params.id });
+    const dish = await Dish.findOne({ _id: req.params.id })
     if (!dish) {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
 
     if (dish.chefId.toString() !== req.user._id.toString()) {
-      throw new Error('Unauthorized');
+      throw new Error('Unauthorized')
     }
-    const data = await Dish.deleteOne({ _id: req.params.id });
+    const data = await Dish.deleteOne({ _id: req.params.id })
     res.status(200).json({
       status: 'success',
       error: '',
-      data,
-    });
+      data
+    })
   } catch (error) {
-    const code = error.message === 'Unauthorized' ? 403 : 400;
+    const code = error.message === 'Unauthorized' ? 403 : 400
     return res.status(code).json({
       status: 'fail',
-      error: error.message,
-    });
+      error: error.message
+    })
   }
-};
+}
 
 // @Usman - Jun 27 12:02
 exports.toggle_like = async (req, res) => {
   try {
-    const dish = await Dish.findOne({ _id: req.params.id });
+    const dish = await Dish.findOne({ _id: req.params.id })
     if (!dish) {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
-    const userId = req.user._id.toString();
-    const isLiked = dish.likes.includes(userId);
+    const userId = req.user._id.toString()
+    const isLiked = dish.likes.includes(userId)
     if (isLiked) {
-      dish.likes = dish.likes.filter((id) => userId !== id);
+      dish.likes = dish.likes.filter((id) => userId !== id)
     } else {
-      dish.likes.push(userId);
+      dish.likes.push(userId)
     }
-    await dish.save();
-    const d = PublicResponse.dish(dish, req);
+    await dish.save()
+    const d = PublicResponse.dish(dish, req)
     res.status(200).json({
       status: 'success',
       error: '',
       data: {
-        dish: d,
-      },
-    });
+        dish: d
+      }
+    })
   } catch (error) {
-    const code = error.message === 'Not found' ? 404 : 400;
+    const code = error.message === 'Not found' ? 404 : 400
     return res.status(code).json({
       status: 'fail',
-      error: error.message,
-    });
+      error: error.message
+    })
   }
-};
+}
 
 // @Usman Jun 27
 exports.toggle_favorite = async (req, res) => {
   try {
-    const dishId = req.params.id;
-    const dish = await Dish.findOne({ _id: dishId });
+    const dishId = req.params.id
+    const dish = await Dish.findOne({ _id: dishId })
     if (!dish) {
-      throw new Error('Not found');
+      throw new Error('Not found')
     }
     const me = await Profile.findOne({
-      userId: req.user._id,
-    });
-    const isFavorite = me.favourites && me.favourites.includes(dishId);
+      userId: req.user._id
+    })
+    const isFavorite = me.favourites && me.favourites.includes(dishId)
 
     if (isFavorite) {
-      me.favourites = me.favourites.filter((id) => id.toString() !== dishId);
+      me.favourites = me.favourites.filter((id) => id.toString() !== dishId)
     } else {
-      me.favourites.push(dishId);
+      me.favourites.push(dishId)
     }
-    me.save();
+    me.save()
 
     return res.status(200).json({
       status: 'success',
       error: '',
       data: {
-        dish: PublicResponse.dish(dish, req, { isFavorite: !isFavorite }),
-      },
-    });
+        dish: PublicResponse.dish(dish, req, { isFavorite: !isFavorite })
+      }
+    })
   } catch (error) {
     return res.status(400).json({
       status: 'fail',
-      error: error.message,
-    });
+      error: error.message
+    })
   }
-};
+}
