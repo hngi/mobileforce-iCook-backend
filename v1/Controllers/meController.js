dd81f5d2eebc8580f92a59c2aea1cfdc539ca684
Chia Gabriel <chiagabrielson@gmail.com>
2020-06-29 23:21:42 +0100

    Fixes #50

--- v1/Controllers/meController.js
+++ v1/Controllers/meController.js
@@ -1,10 +1,45 @@
 const mongoose = require('mongoose')
+const Jimp = require('jimp');
+const multer = require('multer') 
 const User = require('../../Models/authModel')
 const Dish = require('../../Models/dishModel')
 const uploadImage = require('../../Database/uploadImage')
 const Profile = require('../../Models/profileModel')
 const PublicResponse = require('../../Helpers/model')
 
+const multerStorage = multer.memoryStorage()
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true)
+  }else {
+    cb(new Error('Not an image! Please upload only images', false))
+  }
+}
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+})
+
+exports.uploadUserPhoto = upload.single('photo'),
+
+exports.resizeUserPhoto = async (req, res, next) => {
+  try{
+    if (!req.file) return next()
+  
+    req.file.filename = `user-${req.user._id}-${Date.now()}.jpeg`
+  
+    let image = await Jimp.read(req.file.buffer);
+      image.resize(256, 256)
+          .quality(90)
+          .write(`./public/img/users/${req.file.filename}`);
+  
+      next();
+  }catch(err) {
+    throw new Error("AM error occured! Please try again");
+  }
+}
 // @Usman Jun 28
 exports.get_me = async (req, res) => {
   try {
@@ -111,13 +146,14 @@ exports.get_favourites = async (req, res) => {
 exports.get_settings = async (req, res, next) => {}
 
 exports.update_profile = async (req, res) => {
+
   const { name, email, gender, phone } = req.body
   const fieldsToUpdate = {}
   if (name) fieldsToUpdate.name = name
   if (gender) fieldsToUpdate.gender = gender
   if (phone) fieldsToUpdate.phoneNumber = phone
   if (email) fieldsToUpdate.email = email
-
+  if (req.file) fieldsToUpdate.userImage = req.file.filename;
   try {
     let userProfile = await Profile.findOne({ userId: req.user._id })
 
@@ -152,4 +188,6 @@ exports.unlink_facebook = async (req, res) => {}
 
 exports.delete_account = async (req, res) => {}
 
-exports.upload_photo = async (req, res) => {}
+exports.upload_photo = async (req, res) => {
+
+}
